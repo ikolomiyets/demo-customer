0294802e5d9a3688fbcb6b2692c05c7332821bed
Igor Kolomiyets <ikolomiyets@gmail.com>
2017-11-16 09:09:09 +0000

    Initial version

---
+++ routes/customer.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+const customers = {
+    1: {
+        firstName: 'Sarah',
+        lastName: 'Brennan',
+        address: 'Croom, Co. Limerick'
+    },
+    2: {
+        firstName: 'Eva',
+        lastName: 'Olson',
+        address: '3, Patrick st, Limerick'
+    },
+    4: {
+        firstName: 'James',
+        lastName: 'Brennan',
+        address: 'Croom, Co. Limerick'
+    },
+    16: {
+        firstName: 'Dermot',
+        lastName: 'Finnegan',
+        address: 'Anacotty, Co. Limeric'
+    }
+};
+/* GET users listing. */
+router.get('/customers/:customerId', function(req, res, next) {
+    const customerId = req.params['customerId'];
+    res.header('Content-Type', 'application/json');
+    res.json(customers[customerId]);
+});
+
+module.exports = router;
